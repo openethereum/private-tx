bf67744762de133ff51dcdbee29fcaad03a7bafb
Kirill Pimenov <kirill@pimenov.cc>
2018-03-28 20:23:48 +0200

    Adopted all default solium rules (and fixed violations)

--- contracts/PrivateContract.sol
+++ contracts/PrivateContract.sol
@@ -9,30 +9,50 @@ pragma solidity ^0.4.18;
 
 
 contract PrivateContract {
-    address[] public validators;
-    bytes public state;
-    bytes public code;
-    uint256 public nonce;
+	address[] public validators;
+	bytes public state;
+	bytes public code;
+	uint256 public nonce;
 
-    function PrivateContract(address[] initialValidators, bytes initialCode, bytes initialState) public {
-        validators = initialValidators;
-        code = initialCode;
-        state = initialState;
-        nonce = 1;
-    }
-    
-    function getValidators() public constant returns (address[]) {
-        return validators;
-    }
+	function PrivateContract(
+		address[] initialValidators,
+		bytes initialCode,
+		bytes initialState
+	)
+		public
+	{
+		validators = initialValidators;
+		code = initialCode;
+		state = initialState;
+		nonce = 1;
+	}
 
-    function setState(bytes newState, uint8[] v, bytes32[] r, bytes32[] s) public {
-        var noncedStateHash = keccak256([keccak256(newState), bytes32(nonce)]);
+	function getValidators() public constant returns (address[]) {
+		return validators;
+	}
 
-        for (uint i = 0; i < validators.length; i++) {
-            assert(ecrecover(noncedStateHash, v[i], r[i], s[i]) == validators[i]);
-        }
+	function setState(
+		bytes newState,
+		uint8[] v,
+		bytes32[] r,
+		bytes32[] s
+	)
+		public
+	{
+		var noncedStateHash = keccak256([keccak256(newState), bytes32(nonce)]);
 
-        state = newState;
-        nonce = nonce + 1;
-    }
+		for (uint i = 0; i < validators.length; i++) {
+			assert(
+				ecrecover(
+					noncedStateHash,
+					v[i],
+					r[i],
+					s[i]
+				) == validators[i]
+			);
+		}
+
+		state = newState;
+		nonce = nonce + 1;
+	}
 }
